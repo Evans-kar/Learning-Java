d7903806ecf7796523de842a01a514db78d09337
Evans Karuri <duneevanson6@gmail.com>
2026-02-03 07:12:26 -0500

    First Java program - PersonalInformation

---
+++ src/PersonalInformation.java
@@ -0,0 +1,8 @@
+public class PersonalInformation {
+
+    public static void main(String[] args) {
+        System.out.println("Evans Karuri");
+        System.out.println("336 Moi Drive");
+        System.out.println("Nairobi, Kenya 00100");
+    }
+}
