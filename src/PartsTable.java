2da656b279360576c814a3b4d7d0727238f5bf23
Evans Karuri <duneevanson6@gmail.com>
2026-02-03 07:49:54 -0500

    day 1

---
+++ src/PartsTable.java
@@ -0,0 +1,10 @@
+public class PartsTable {
+
+    public static void main(String[] args) {
+        System.out.println("PART NO\tPRICE");
+        System.out.println("T1267\t$6.34");
+        System.out.println("T1300\t$8.92");
+        System.out.println("T2401\t$65.40");
+        System.out.println("T4482\t$36.99");
+    }
+}
