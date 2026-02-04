e5dfb3066aefc310f9b98a86c36537e8fb7bc6b7
Evans Karuri <duneevanson6@gmail.com>
2026-02-04 07:32:42 -0500

    Learned about swing package in java and practised with a little program

---
+++ src/DisplayADialog.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class DisplayADialog {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello World!",
+                "Sample",  JOptionPane.WARNING_MESSAGE);
+        System.exit(0);
+    }
+}
