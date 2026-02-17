3f668e8ad69488ececbcbfc8dccd2b4d72403838
Evans Karuri <duneevanson6@gmail.com>
2026-02-17 04:34:53 -0500

    Functions and functions invocation

--- src/functionsJavascript.js
+++ src/functionsJavascript.js
@@ -1,25 +1,4 @@
-function addTwoNumbers(a,b) {
-    return a+b;
+function addTwoAndSeven() {
+    return 2 + 7;
 }
-let sum1 = addTwoNumbers(3,5);
-let sum2 = addTwoNumbers(5,5);
-console.log(sum1 + sum2);
-
-function greetings(name = "Guests") {
-    console.log(`Hello ${name}!`);
-}
-greetings();
-greetings("Anna");
-greetings("John");
-
-function mystery(a, b=3) {
-    return a * b;
-}
-console.log(mystery(4));
-
-//function expressions example
-const add = function (a, b) {
-    return a + b;
-}
-let z = add(5,28)
-console.log(z);
+console.log(addTwoAndSeven);
